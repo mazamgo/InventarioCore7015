cacf31cfdf4d9d4204d33e3d63f1751d575d3ae1
mazamgo <mauricio.zambrana.g@gmail.com>
2024-02-14 16:30:31 -0600

    Agregando configuraciones

---
+++ AccessoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Modelos;
+using System.Reflection;
+
+namespace AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
---
+++ Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Nombre es requerido.")]
+        [MaxLength(60,ErrorMessage = "Nombre no debe ser mayor a 60 caracteres.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripcio es requerido.")]
+        [MaxLength(100, ErrorMessage = "Descripcion no debe ser mayor a 100 caracteres.")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Estado es requerido.")]
+        public bool Estado { get; set; }
+    }
+}
